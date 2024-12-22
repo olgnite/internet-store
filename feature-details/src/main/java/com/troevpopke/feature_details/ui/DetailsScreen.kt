b6ccd253a014bc29c26e9f2750e38d3a6dbc7e46
rustam41k0 <rustamskysan@gmail.com>
2024-12-22 17:27:31 +0500

    Header для детальной карточки товара

--- feature-details/src/main/java/com/troevpopke/feature_details/ui/DetailsScreen.kt
+++ feature-details/src/main/java/com/troevpopke/feature_details/ui/DetailsScreen.kt
@@ -9,8 +9,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -38,16 +50,33 @@ fun DetailsScreen(
         is DetailsViewModel.State.Content -> {
             ProductCard(
                 product = state.product,
+                onBackClick = onBackClick
             )
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductCard(
     product: Product,
-) {
-    Scaffold() {
+    onBackClick: () -> Unit,
+    ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Troe V Popke Shop") },
+                navigationIcon = {
+                    IconButton(onClick = { onBackClick() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+            )
+        }
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
