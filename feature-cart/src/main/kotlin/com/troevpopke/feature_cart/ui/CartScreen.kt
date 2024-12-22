ad6456a750ccef74e3f426a195b535d556adbc2d
DESKTOP-DVU5R3V\olgnite <lickotyn2@yandex.ru>
2024-12-22 16:43:34 +0500

    верстка корзины

--- feature-cart/src/main/kotlin/com/troevpopke/feature_cart/ui/CartScreen.kt
+++ feature-cart/src/main/kotlin/com/troevpopke/feature_cart/ui/CartScreen.kt
@@ -1,8 +1,13 @@
 package com.troevpopke.feature_cart.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.troevpopke.common.models.Product
+import java.lang.reflect.Modifier
 
 @Composable
 fun CartScreen(
@@ -10,7 +15,24 @@ fun CartScreen(
     onAddProduct: (product: Product) -> Unit,
     onRemoveProduct: (id: String) -> Unit
 ) {
+    Cart(
+        viewModel.state,
+        onAddProduct,
+        onRemoveProduct
+    )
+}
 
+@Composable
+fun Cart(
+    products: List<Product>,
+    onAddProduct: (product: Product) -> Unit,
+    onRemoveProduct: (id: String) -> Unit
+) {
+    OutlinedCard() {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {  }
+    }
 
 
 }
--- feature-home/src/main/java/com/troevpopke/feature/home/presentation/HomeScreen.kt
+++ feature-home/src/main/java/com/troevpopke/feature/home/presentation/HomeScreen.kt
@@ -28,13 +28,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
 import com.troevpopke.common.models.Product
-import com.troevpopke.common.ui.theme.FinalProjectTheme
 import com.troevpopke.feature.home.viewmodel.ProductViewModel
 
 @Composable
